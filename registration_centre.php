1b94dafa68358a0d3ef838a01a29cbb604c0e771
Vedprakash Poddar <ved.prakash@visionias.in>
2019-05-21 12:33:57 +0530

    Vision Centers all files

---
+++ registration_centre.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en" style="height:100%;">
+<head>
+	<title>VisionIAS - Home</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="google-signin-scope" content="profile email">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="/office/css/header.css" />
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+	<link rel="shortcut icon" href="../../student/images/favicon_vision.ico">
+	<style>
+</style>	
+</head>
+<body>
+<!-- A grey horizontal navbar that becomes vertical on small screens -->
+<nav class="navbar custom navbar-expand-sm hide-on-small fixed-top"> 	
+
+  <!-- Links -->
+  <ul class="navbar-nav mr-auto ">
+    <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fa fa-phone fa-flip-horizontal"></i> +918468022022</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"><img src="/office/images/android-character-symbol.svg" height="17" width="17"/> DOWNLOAD APP</a>
+    </li>
+  </ul>
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="#">CLASSROOM DEMO</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">OPEN TEST</a>
+    </li>
+  </ul>
+</nav>
+<!--Nav bar-->
+<nav class="navbar custom-main-nev navbar-expand-md bg-light navbar-light fixed-top fixed-top-2">
+  <!-- Brand -->
+  <a class="navbar-brand" href="#"><img src="/office/images/Vision.png" height="33" width="160"/></a>
+
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link nav-text" href="#">TEST SERIES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link nav-text" href="#">CLASSROOM</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link nav-text" href="#">INTERVIEW</a>
+      </li> 
+	  <li class="nav-item">
+        <a class="nav-link nav-text" href="#">RESOURCES</a>
+      </li> 
+	  <li class="nav-item">
+        <a class="nav-link nav-text" href="#">LOGIN</a>
+      </li> 
+	  <li class="nav-item">
+        <a class="nav-link nav-text" href="#">REGISTER</a>
+      </li> 
+    </ul>
+  </div> 
+</nav>
+<!--End of nav bar-->
+<div class="container">
+  <h3 style="text-align:center">REGISTRATION CENTRES</h2>
+  <div class="row row-eq-height">
+  <div class="col-sm-6 col-md-4">
+    <div class="card">
+	  <div class="card-header"> HEAD OFFICE</div>
+      <div class="card-body">
+        <p class="card-text">
+		<strong><i class="far fa-building"></i></strong>
+        2nd floor, Apsara Arcade, Near Gate-7 Karol Bagh Metro Station, 1/8 b, Pusa Road, Karol Bagh, Delhi – 110005</p>
+		<p class="card-text">
+		<strong>
+          <i class="fas fa-envelope"></i></strong> enquiry@visionias.in, info@visionias.in</p>
+		<p class="card-text">
+		<strong>
+          <i class="fa fa-phone fa-flip-horizontal"></i></strong>
+        +91 8468022022, +91 9650617807, +91 9717162595</p>
+        <a href="https://www.google.com/maps/place/Vision+IAS/@28.6435615,77.1888234,20z/data=!4m13!1m7!3m6!1s0x390d029f555cf7ef:0x9a0df6691dfbff54!2sApsara+Arcade,+Pusa+Rd,+North+Extension+Area,+Rajinder+Nagar,+New+Delhi,+Delhi+110060,+India!3b1!8m2!3d28.6435877!4d77.1888786!3m4!1s0x0:0xafd0ea4129681340!8m2!3d28.643711!4d77.188837" class="btn btn-danger center-block" target="black"><i class="fas fa-map-marker-alt"></i> Google Map </a>
+      </div>
+    </div>
+	</div>
+  <div class="col-sm-6 col-md-4">
+    <div class="card">
+	  <div class="card-header"> MUKHRJEE NAGAR CENTRE</div>
+      <div class="card-body">
+        <p class="card-text">
+		<strong><i class="far fa-building"></i></strong>
+        635, Opposite Signature View Apartments, Banda Bahadur Marg, Mukherjee Nagar, Delhi –110009
+		</p>
+		<p class="card-text">
+		<strong>
+          <i class="fas fa-envelope"></i></strong> enquiry@visionias.in, info@visionias.in</p>
+		<p class="card-text">
+		<strong>
+          <i class="fa fa-phone fa-flip-horizontal"></i></strong>
+        +91 8468022022, +91 9650617807, +91 9717162595</p>
+        <a href="https://www.google.co.in/maps/place/Vision+IAS/@28.7098137,77.2135129,19z/data=!4m8!1m2!2m1!1svision+ias!3m4!1s0x390cfde11ddd78fb:0xab99bab87cd8551a!8m2!3d28.709747!4d77.213867?hl=en" class="btn btn-danger center-block" target="black"><i class="fas fa-map-marker-alt"></i> Google Map </a>
+      </div>
+    </div>
+	</div>
+	<div class="col-sm-6 col-md-4">
+    <div class="card">
+	  <div class="card-header"> HYDERABAD CENTRE</div>
+      <div class="card-body">
+        <p class="card-text">
+		<strong><i class="far fa-building"></i></strong>
+        1-10-140/A, 3rd Floor, Rajamani Chambers, ST.NO.8, Ashok Nagar, Hyderabad, Telangana - 500020</p>
+		<p class="card-text">
+		<strong>
+          <i class="fas fa-envelope"></i></strong> hyderabad@visionias.in</p>
+		<p class="card-text">
+		<strong>
+          <i class="fa fa-phone fa-flip-horizontal"></i></strong>
+		  +91 4066205656, +91 9000104133, +91 9494374078, +91 9799974032
+        </p>
+        <a href="https://www.google.com/maps/place/Vision+IAS+Hydrabad+Center/@17.4078255,78.4898754,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0xac39fcb5215efbae?hl=en-IN" class="btn btn-danger center-block" target="black"><i class="fas fa-map-marker-alt"></i> Google Map </a>
+      </div>
+    </div>
+	</div>
+  <div class="col-sm-6 col-md-4">
+    <div class="card">
+	  <div class="card-header"> PUNE CENTRE</div>
+      <div class="card-body">
+        <p class="card-text">
+		<strong><i class="far fa-building"></i></strong>
+        Office No. 403, Eiffel Square, Near Shakti Sports, Tilak Road, Pune - 411030
+		</p>
+		<p class="card-text">
+		<strong>
+          <i class="fas fa-envelope"></i></strong> pune@visionias.in</p>
+		<p class="card-text">
+		<strong>
+          <i class="fa fa-phone fa-flip-horizontal"></i></strong>
+        +91 8007500096, +91 7219498840</p>
+        <a href="https://goo.gl/maps/RQ1nMbvmBNM2" class="btn btn-danger center-block" target="black"><i class="fas fa-map-marker-alt"></i> Google Map </a>
+      </div>
+    </div>
+	</div>
+  <div class="col-sm-6 col-md-4">
+    <div class="card">
+	  <div class="card-header"> JAIPUR CENTRE</div>
+      <div class="card-body">
+        <p class="card-text">
+		<strong><i class="far fa-building"></i></strong>
+        VISION IAS (ANOOP KR SINGH) 119, Apex Mall, Tonk Road, Jaipur-302015
+		</p>
+		<p class="card-text">
+		<strong>
+          <i class="fas fa-envelope"></i></strong> jaipur@visionias.in</p>
+		<p class="card-text">
+		<strong>
+          <i class="fa fa-phone fa-flip-horizontal"></i></strong>
+        +91 9001949244, +91 9799974032</p>
+        <a href="https://www.google.com/maps/place/Apex+Mall,+Sawai+Mansingh+Rd,+Greater+Kailash+Colony,+Vidhayak+Nagar,+Lalkothi,+Jaipur,+Rajasthan+302015,+India/@26.889227,75.8032589,17z/data=!3m1!4b1!4m2!3m1!1s0x396db426e88c1eaf:0x1bacb050a8d76a8" class="btn btn-danger center-block" target="black"><i class="fas fa-map-marker-alt"></i> Google Map </a>
+      </div>
+    </div>
+	</div>
+	<div class="col-sm-6 col-md-4">
+    <div class="card">
+	  <div class="card-header"> AHMEDABAD CENTRE</div>
+      <div class="card-body">
+        <p class="card-text">
+		<strong>
+		<i class="far fa-building"></i></strong>
+        101, First Floor, Addor Ambition,Near Navkar Institute, Navrang School Circle, Navrangpura, Ahmedabad- 380009</p>
+		<p class="card-text">
+		<strong>
+          <i class="fas fa-envelope"></i></strong> ahmedabad@visionias.in</p>
+		<p class="card-text">
+		<strong>
+          <i class="fa fa-phone fa-flip-horizontal"></i></strong>
+		  +91 9909447040, +91 7575007040, +91 7948997040
+        </p>
+        <a href="https://www.google.co.in/maps/place/Vision+IAS/@23.0464669,72.5583156,15z/data=!4m5!3m4!1s0x0:0x4870878b18220214!8m2!3d23.0464669!4d72.5583156" class="btn btn-danger center-block" target="black"><i class="fas fa-map-marker-alt"></i> Google Map </a>
+      </div>
+    </div>
+	</div>
+</div>
+</div>
+</body>
+</html>
